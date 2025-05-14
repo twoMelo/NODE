56ff9aa2bedec3b3e03f265b40824a3924cad97f
twoMelo <mmeloramalho@gmail.com>
2025-05-14 13:16:59 -0300

    .

--- Conversor de Moedas/servidor.js
+++ Conversor de Moedas/servidor.js
@@ -1,50 +1,49 @@
+// servidor.js
 const express = require("express")
-const fetch = require("node-fetch")
 const path = require("path")
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
 
 const app = express()
 const porta = 3000
 
 // Chave da API
-const API_KEY = "37e81c7b67994fa2ea1e631805122858"
+const API_KEY = "fed2fd239a7be26492779d05cbc66eb3"
 
 // Pegar os arquivos da pasta public
 app.use(express.static("public"))
 
-// Rota para converter a moeda
-//req e res = requisição e resposta
+//Rota para converter a moeda
+//req e res = requisição e reposta
 app.get("/converter", async (req, res) => {
     try {
         const {valor, de, para} = req.query
 
         if(!valor || !de || !para) {
-            return res.status(400).json({erro: "Parâmetro inválido"})
+            return res.status(400).json({erro: "Parâmetro Inválido"})
         }
-
         console.log(`Fazendo requisição API: ${de} -> ${para}, valor: ${valor}`)
-        const url = `http://api.exchangerate.host/convert?access_key=${API_KEY}&from=${de}&to=${para}&amount=${valor}`
+        const url = `https://exchangerate.host/dashboard?reset_access_key=1${API_KEY}&from=${de}&to=${para}&amount=${valor}`
         console.log("URL DA API", url)
 
-        const resposta = await fetch(url)
-        const dados = await resposta.json()
+        const reposta = await fetch(url)
+        const dados = await reposta.json()
 
         console.log("Resposta da API", dados)
 
         if(!dados.success) {
-            throw new Error(`Erro na API ${dados.error?.info || "Erro desconhecido"}`)
+            throw new Error(`Erro na API ${dados.error?.info ||"Erro desconhecido"}`)
         }
         res.json({
             valorConvertido: dados.result
         })
-    } catch (erro) {
+    } catch(erro){
         console.error("Erro detalhado", erro)
         res.status(500).json({
-            erro: "Erro ao converter moeda",
+            erro:"Erro ao converter moeda",
             detalhes: erro.message
         })
     }
 })
-applisten(porta, () => {
-    console.log(`Servidor rodando em: http://localhost:${porta}`)
-
+app.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`)
 })
