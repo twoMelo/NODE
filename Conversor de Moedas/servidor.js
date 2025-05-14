bd31b1bcf748247008edd8c29fda03176718b750
twoMelo <mmeloramalho@gmail.com>
2025-05-14 08:49:43 -0300

    .

---
+++ Conversor de Moedas/servidor.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const fetch = require("node-fetch")
+const path = require("path")
+
+const app = express()
+const porta = 3000
+
+// Chave API
+const API_KEY = "2aca24a4103abd45157a5e84c8bc7af0"
+
+d
