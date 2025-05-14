5004e528bd4deacfa445ab5dca82b095ed6969ca
twoMelo <mmeloramalho@gmail.com>
2025-05-14 08:58:49 -0300

    .

--- Conversor de Moedas/servidor.js
+++ Conversor de Moedas/servidor.js
@@ -5,7 +5,24 @@ const path = require("path")
 const app = express()
 const porta = 3000
 
-// Chave API
-const API_KEY = "2aca24a4103abd45157a5e84c8bc7af0"
+// Chave da API
+const API_KEY = "37e81c7b67994fa2ea1e631805122858"
 
-d
+// Pegar os arquivos da pasta public
+app.use(express.static("public"))
+
+// Rota para converter a moeda
+//req e res = requisição e resposta
+app.get("/converter", async (req, res) => {
+    try {
+        const {valor, de, para} = req.query
+
+        if(!valor || !de || !para) {
+            return res.status(400).json({erro: "Parâmetro inválido"})
+        }
+
+        console.log(`Fazendo requisição API: ${de} -> ${para}, valor: ${valor}`)
+        const url = `http://api.exchangerate.host/convert?access_key=${API_KEY}&from=${de}&to=${para}&amount=${valor}`
+        console.log("URL DA API", url)
+    }
+})
